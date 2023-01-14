4fdefac8d0ce9167cd4cb2cf5897b28b5548fc2b
Pavel Syu <poet.poet@gmail.com>
2023-01-15 00:38:25 +0300

    added log4j

--- src/main/java/org/example/web/controllers/HomeController.java
+++ src/main/java/org/example/web/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package org.example.web.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +9,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+private Logger logger = Logger.getLogger(HomeController.class);
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public ModelAndView home(){
+        logger.info("GET /home returns index.html");
         return new ModelAndView("index");
 
     }
