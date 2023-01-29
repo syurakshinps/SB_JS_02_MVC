0513e358e65a46e7c416ab1c1af153c37c44aa57
Pavel Syu <poet.poet@gmail.com>
2023-01-29 16:16:38 +0300

    2.6 Загрузка и скачивание файлов в Spring MVC done

--- src/main/java/org/example/web/controllers/BookShelfController.java
+++ src/main/java/org/example/web/controllers/BookShelfController.java
@@ -13,8 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 @Controller
 @RequestMapping(value = "books")
@@ -77,4 +81,30 @@ public class BookShelfController {
         }
     }
 
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("file")MultipartFile file) throws Exception{
+        String name = file.getOriginalFilename();
+        byte[] bytes = file.getBytes();
+
+        //create dir
+        String rootPath = System.getProperty("catalina.home");
+        File dir = new File(rootPath + File.separator + "external_uploads");
+        if (!dir.exists()){
+            dir.mkdirs();
+        }
+
+        //create file
+        File serverFile = new File(dir.getAbsolutePath() + File.separator + name);
+        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+        stream.write(bytes);
+        stream.close();
+
+        logger.info("saved new file: " + serverFile.getAbsolutePath());
+
+        return "redirect:/books/shelf";
+
+
+
+    }
 }
